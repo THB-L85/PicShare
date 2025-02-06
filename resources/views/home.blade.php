f003e744499ceca09cd5bdbaf94f0d931801b5e3
THB-L85 <luis.dr85a@gmail.com>
2025-02-05 23:34:56 -0600

    Subiendo proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title')
+    <title> PicShare </title>
+@endsection
+@section('content')
+    <div class=" bg-red-800 text-white">Hola, Tailwind añadido</div>
+@endsection
